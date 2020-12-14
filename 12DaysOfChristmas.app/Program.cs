5e90a2f0ce1ab081cad4866b2bccee188f15ce44
zacstalbow <72789151+zacstalbow@users.noreply.github.com>
2020-12-14 12:07:21 +0000

    Add files via upload

---
+++ 12DaysOfChristmas.app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12DaysOfChristmas.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day = new string[12] { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
+
+            string[] present = new string[12] { "A partridge in a pear tree", "Two turtle doves,", "Three French hens,", "Four calling birds,", "Five golden rings,", 
+                "Six eese a-laying,", "Seven swans a-swimming,", "Eight maids a-milking,", "Nine ladies dancing,", "Ten lords a-leaping,", "Eleven pipers piping,", "Twelve drummers drumming," };
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"On the {day[i]} day of Christmas, my true love sent to me ");
+
+                int n = i + 1;
+                while (n-- > 0)
+                    Console.WriteLine(present[n]);
+                    Console.WriteLine();
+
+                if (i == 0)
+                    present[0] = "And a partridge in a pear tree.";
+            }
+        }
+
+    }
+}
---
+++ 12DaysOfChristmas.app/Program/README.md
@@ -0,0 +1,11 @@
+# Christmas Challenge - Code Golf 
+
+Write a program to print out the carol ["The Twelve days of Christmas"](https://www.christmas-carols.net/carols/twelve-days.html) for each and every one of the twelve days. 
+
+Your program should print out all twelve days, *in full including accurate punctuation*, please! 
+
+You should look to use iteration, probably a list or two, maybe some conditional statements too. 
+
+**Bonus**: a prize (in the New Year) for the shortest working code, i.e. the source code file using the fewest characters. And, for the avoidance of doubt, putting them in a text file is cheating!!
+
+Can you get it working before tomorrow's lesson?! 
